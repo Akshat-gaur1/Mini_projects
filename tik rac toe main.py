32fad46b29503d7efce12eaf9668e108e2f2e7e6
Akshat <154447242+Akshat-gaur1@users.noreply.github.com>
2024-07-09 14:02:27 +0530

    tik tac toe.py

--- tik rac toe main.py
+++ tik rac toe main.py
@@ -1,127 +1,75 @@
 import random
 
-print ("----------------------------")
+print("----------------------------")
+# Print a separator line
+possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-possibleNumbers = [1,2,3,4,5,6,7,8,9]
+gameBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Initialize the game board in a matrix
+rows = 3
+columns = 3
 
-gameBoard = [[1,2,3],[4,5,6],[7,8,9]] 
-rows = 3 
-columns  =  3
-
+# Function to print the game board it'll be a matrix for tic tac toe
 def printGameBoard():
     for x in range(rows):
-        print ("\n+---+---+---+")
-        print ("|", end = "" )
+        print("\n+----+----+----+")
+        print("|", end="")
         for y in range(columns):
-            print ("", gameBoard[x][y], end = " | ")
-    print ("\n+---+---+---+")
-            
-printGameBoard()
-
-
+            print(" ", gameBoard[x][y], end=" |")
+    print("\n+----+----+----+")
+# Function to modify the game board based on user input
 def modifyArray(num, turn):
     num -= 1
-    if(num == 0):
-        gameBoard[0][0] = turn
-    elif(num == 1):
-        gameBoard[0][1] = turn
-    elif(num == 2):
-        gameBoard[0][2] = turn
-    elif(num == 3):
-        gameBoard[1][0] = turn
-    elif(num == 4):
-        gameBoard[1][1] = turn
-    elif(num == 5):
-        gameBoard[1][2] = turn
-    elif(num == 6):
-        gameBoard[2][0] = turn
-    elif(num == 7):
-        gameBoard[2][1] = turn
-    elif(num == 8):
-        gameBoard[2][2] = turn
+    gameBoard[num // 3][num % 3] = turn
 
-### Define function to check for a winner
-def checkForWinner(gameBoard):
-### X axis
-    if(gameBoard[0][0] == 'X' and gameBoard[0][1] == 'X' and gameBoard[0][2] == 'X'):
-        print("X has won!")
-        return "X"
-    elif(gameBoard[0][0] == 'O' and gameBoard[0][1] == 'O' and gameBoard[0][2] == 'O'):
-        print("O has won!")
-        return "O"
-    elif(gameBoard[1][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[1][2] == 'X'):
-        print("X has won!")
-        return "X"
-    elif(gameBoard[1][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[1][2] == 'O'):
-        print("O has won!")
-        return "O"
-    elif(gameBoard[2][0] == 'X' and gameBoard[2][1] == 'X' and gameBoard[2][2] == 'X'):
-        print("X has won!")
-        return "X"
-    elif(gameBoard[2][0] == 'O' and gameBoard[2][1] == 'O' and gameBoard[2][2] == 'O'):
-        print("O has won!")
-    return "O"
-### Y axis
-    if(gameBoard[0][0] == 'X' and gameBoard[1][0] == 'X' and gameBoard[2][0] == 'X'):
-        print("X has won!")
-        return "X"
-    elif(gameBoard[0][0] == 'O' and gameBoard[1][0] == 'O' and gameBoard[2][0] == 'O'):
-        print("O has won!")
-        return "O"
-    elif(gameBoard[0][1] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][1] == 'X'):
-        print("X has won!")
-        return "X"
-    elif(gameBoard[0][1] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][1] == 'O'):
-        print("O has won!")
-        return "O"
-    elif(gameBoard[0][2] == 'X' and gameBoard[1][2] == 'X' and gameBoard[2][2] == 'X'):
-        print("X has won!")
-        return "X"
-    elif(gameBoard[0][2] == 'O' and gameBoard[1][2] == 'O' and gameBoard[2][2] == 'O'):
-        print("O has won!")
-        return "O"
-### Cross wins
-    elif(gameBoard[0][0] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][2] == 'X'):
-        print("X has won!")
-        return "X"
-    elif(gameBoard[0][0] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][2] == 'O'):
-        print("O has won!")  
-        return "O"
-    elif(gameBoard[0][2] == 'X' and gameBoard[1][1] == 'X' and gameBoard[2][0] == 'X'):
-        print("X has won!")  
-        return "X"
-    elif(gameBoard[0][2] == 'O' and gameBoard[1][1] == 'O' and gameBoard[2][0] == 'O'):
-        print("O has won!") 
-        return "O" 
-    else:
-        return "N"
+def checkForWinner(board):
+    # Check rows and columns for victory (rules fro winning)
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2]:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i]:
+            return board[0][i]
+    # Check diagonals for victory
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+    # when no winners found
+    return None
+
+def isBoardFull(board):
+    return all(isinstance(cell, str) for row in board for cell in row)
 
 leaveLoop = False
 turnCounter = 0
-
-while(leaveLoop == False):
-    ### It's the player turn
-    if(turnCounter % 2 == 0):
-        printGameBoard()
+# main game
+while not leaveLoop:
+    printGameBoard()
+    if turnCounter % 2 == 0:
+#players turn to choose an number to mark in order to win
         numberPicked = int(input("\nChoose a number [1-9]: "))
-    if(numberPicked >= 1 or numberPicked <= 9):
-        modifyArray(numberPicked, 'X')
-        possibleNumbers.remove(numberPicked)
+        if numberPicked in possibleNumbers:
+            modifyArray(numberPicked, 'X')
+            possibleNumbers.remove(numberPicked)
+            turnCounter += 1
+        else:
+            print("Invalid input or cell already taken. Please try again.")
     else:
-        print("Invalid input. Please try again.")
-    turnCounter += 1
-### It's the computer's turn
-else:
-        while(True):
-            cpuChoice = random.choice(possibleNumbers)
-            print("\nCpu choice: ", cpuChoice)
-            if(cpuChoice in possibleNumbers):
-                modifyArray(cpuChoice, 'O')
-                possibleNumbers.remove(cpuChoice)
-                turnCounter += 1
-                break
+        cpuChoice = random.choice(possibleNumbers)
+        print("\nCpu choice:", cpuChoice)
+        modifyArray(cpuChoice, 'O')
+        possibleNumbers.remove(cpuChoice)
+        turnCounter += 1
+    
+    winner = checkForWinner(gameBoard)
+    if winner:
+        printGameBoard()
+        print(f"\n{winner} has won!")
+        leaveLoop = True
+# to check if it's a draw
+    elif isBoardFull(gameBoard):
+        printGameBoard()
+        print("\nIt's a draw!")
+        leaveLoop = True
 
-        winner = checkForWinner(gameBoard)
-        if(winner != "N"):
-            print("\nGame over! Thank you for playing :)")
-            break
+print("\nGame over! Thank you for playing :)")
